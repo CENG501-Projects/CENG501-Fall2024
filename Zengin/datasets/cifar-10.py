059fea57da5596b23690a4d3855b4ad57db85dc9
Kerem Zengin <161139738+kerem-z@users.noreply.github.com>
2024-11-24 11:54:47 +0300

    minor fix on data fetching codes

--- Zengin/datasets/cifar-10.py
+++ Zengin/datasets/cifar-10.py
@@ -2,7 +2,7 @@ import torchvision.datasets as datasets
 import os
 
 def download_cifar10(destination="./datasets/cifar10"):
-    os.makedirs(destination, exist_ok=True)
+    os.makedirs(destination, exist_ok=False)
     datasets.CIFAR10(destination, download=True)
 
 if __name__ == "__main__":
