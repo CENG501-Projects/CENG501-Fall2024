b85a6b0b22ebb064d3d4b03a9312e54ddcbaebb1
Kerem Zengin <161139738+kerem-z@users.noreply.github.com>
2024-12-04 15:13:29 +0300

    delete datafetching codes

--- Zengin/datasets/cifar-10.py
+++
@@ -1,9 +0,0 @@
-import torchvision.datasets as datasets
-import os
-
-def download_cifar10(destination="./datasets/cifar10"):
-    os.makedirs(destination, exist_ok=False)
-    datasets.CIFAR10(destination, download=True)
-
-if __name__ == "__main__":
-    download_cifar10()
