9d7bcc399ae4cb04f3f68ca50299ef63b31640ef
erinc <burakerinc@gmail.com>
2024-12-08 14:25:17 +0300

    Add experiment run files

---
+++ CetinSak/src/experiments/__init__.py
@@ -0,0 +1,465 @@
+
+import os
+import argparse
+import time
+import math
+import pickle
+import copy
+from functools import partial
+
+from optim_loss import loss_func, regularize, opt_algo, measure_accuracy
+from utils import cpu_state_dict, args2train_test_sizes
+from observables import locality_measure, state2permutation_stability, state2clustering_error
+
+DEBUG = True
+
+if DEBUG:
+    from src.SRHM.reference_code.hierarchical import RandomHierarchyModel as DataModel
+else:
+    from src.SRHM.srhm import SparseRandomHierarchyModel as DataModel
+
+# Models
+from src.SRHM.cnn import CNN
+from src.SRHM.lcn import LCN
+from src.SRHM.fcn import FCN, FCN2
+
+def experiment_run(args):
+
+    trainloader, testloader, net = experiment_initialization(args)
+
+    with open(args.output, "wb") as handle:
+        pickle.dump(args, handle)
+    try:
+        for data in run(args):
+            with open(args.output, "wb") as handle:
+                pickle.dump(args, handle)
+                pickle.dump(data, handle)
+    except:
+        os.remove(args.output)
+        raise
+
+def run(args):
+
+    # if args.dtype == 'float64':
+    #     torch.set_default_dtype(torch.float64)
+    # if args.dtype == 'float32':
+    #     torch.set_default_dtype(torch.float32)
+    # if args.dtype == 'float16':
+    #     torch.set_default_dtype(torch.float16)
+
+    best_acc = 0  # best test accuracy
+    criterion = partial(loss_func, args)
+
+    trainloader, testloader, net0 = init_fun(args)
+
+    # scale batch size when larger than train-set size
+    if (args.batch_size >= args.ptr) and args.scale_batch_size:
+        args.batch_size = args.ptr // 2
+
+    if args.save_dynamics:
+        dynamics = [{"acc": 0.0, "epoch": 0., "net": cpu_state_dict(net0)}]
+    else:
+        dynamics = None
+
+    loss = []
+    terr = []
+    locality = []
+    stability = []
+    clustering_error = []
+    epochs_list = []
+
+    best = dict()
+    trloss_flag = 0
+
+    for net, epoch, losstr, avg_epoch_time in train(args, trainloader, net0, criterion):
+
+        assert str(losstr) != "nan", "Loss is nan value!!"
+        loss.append(losstr)
+        epochs_list.append(epoch)
+
+        # measuring locality for fcn nets
+        if args.locality == 1:
+            assert args.net == 'fcn', "Locality can only be computed for fcns !!"
+            state = net.state_dict()
+            hidden_layers = [state[k] for k in state if 'w' in k][:-2]
+            with torch.no_grad():
+                locality.append(locality_measure(hidden_layers, args)[0])
+
+        # measure stability to semantically equivalent data realizations
+        if args.stability == 1:
+            state = net.state_dict()
+            stability.append(state2permutation_stability(state, args))
+        if args.clustering_error == 1:
+            state = net.state_dict()
+            clustering_error.append(state2clustering_error(state, args))
+
+        if epoch % 10 != 0 and not args.save_dynamics: continue
+
+        if testloader:
+            acc = test(args, testloader, net, criterion, print_flag=epoch % 5 == 0)
+        else:
+            acc = torch.nan
+        terr.append(100 - acc)
+
+        if args.save_dynamics:
+        #     and (
+        #     epoch
+        #     in (10 ** torch.linspace(-1, math.log10(args.epochs), 30)).int().unique()
+        # ):
+            # save dynamics at 30 log-spaced points in time
+            dynamics.append(
+                {"acc": acc, "epoch": epoch, "net": cpu_state_dict(net)}
+            )
+        if acc > best_acc:
+            best["acc"] = acc
+            best["epoch"] = epoch
+            if args.save_best_net:
+                best["net"] = cpu_state_dict(net)
+            # if args.save_dynamics:
+            #     dynamics.append(best)
+            best_acc = acc
+            print(f"BEST ACCURACY ({acc:.02f}) at epoch {epoch:.02f} !!", flush=True)
+
+        out = {
+            "args": args,
+            "epoch": epochs_list,
+            "train loss": loss,
+            "terr": terr,
+            "locality": locality,
+            "stability": stability,
+            "clustering_error": clustering_error,
+            "dynamics": dynamics,
+            "best": best,
+        }
+
+        yield out
+
+        if (losstr == 0 and args.loss == 'hinge') or (losstr < args.zero_loss_threshold and args.loss == 'cross_entropy'):
+            trloss_flag += 1
+            if trloss_flag >= args.zero_loss_epochs:
+                break
+
+    try:
+        wo = weights_evolution(net0, net)
+    except:
+        print("Weights evolution failed!")
+        wo = None
+
+    if args.locality == 2:
+        assert args.net == 'fcn', "Locality can only be computed for fcns !!"
+        state = net.state_dict()
+        hidden_layers = [state[k] for k in state if 'w' in k][:-2]
+        with torch.no_grad():
+            locality.append(locality_measure(hidden_layers, args)[0])
+
+    if args.stability == 2:
+        state = net.state_dict()
+        stability.append(state2permutation_stability(state, args))
+
+    if args.clustering_error == 2:
+        state = net.state_dict()
+        clustering_error.append(state2clustering_error(state, args))
+
+    out = {
+        "args": args,
+        "epoch": epochs_list,
+        "train loss": loss,
+        "terr": terr,
+        "locality": locality,
+        "stability": stability,
+        "clustering_error": clustering_error,
+        "dynamics": dynamics,
+        "init": cpu_state_dict(net0) if args.save_init_net else None,
+        "best": best,
+        "last": cpu_state_dict(net) if args.save_last_net else None,
+        "weight_evo": wo,
+        'avg_epoch_time': avg_epoch_time,
+    }
+    yield out
+
+
+def train(args, trainloader, net0, criterion):
+
+    net = copy.deepcopy(net0)
+
+    optimizer, scheduler = opt_algo(args, net)
+    print(f"Training for {args.epochs} epochs...")
+
+    start_time = time.time()
+
+    num_batches = math.ceil(args.ptr / args.batch_size)
+    checkpoint_batches = torch.linspace(0, num_batches, 10, dtype=int)
+
+    for epoch in range(args.epochs):
+
+        # layerwise training
+        if epoch % (args.epochs // args.net_layers + 1) == 0:
+            if 'layerwise' in args.net:
+                l = epoch // (args.epochs // args.net_layers + 1)
+                net.init_layerwise_(l)
+                print(f'Layer-wise training up to layer {l}.', flush=True)
+
+        net.train()
+        train_loss = 0
+        correct = 0
+        total = 0
+        for batch_idx, (inputs, targets) in  (trainloader):
+            inputs, targets = inputs.to(args.device), targets.to(args.device)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+
+            loss = criterion(outputs, targets)
+            train_loss += loss.detach().item()
+            regularize(loss, net, args.weight_decay, reg_type=args.reg_type)
+            loss.backward()
+            optimizer.step()
+
+            correct, total = measure_accuracy(args, outputs, targets, correct, total)
+
+            # during first epoch, save some sgd steps instead of after whole epoch
+            if epoch < 10 and batch_idx in checkpoint_batches and batch_idx != (num_batches - 1):
+                yield net, epoch + (batch_idx + 1) / num_batches, train_loss / (batch_idx + 1), None
+
+        avg_epoch_time = (time.time() - start_time) / (epoch + 1)
+
+        if epoch % 5 == 0:
+            print(
+                f"[Train epoch {epoch+1} / {args.epochs}, {print_time(avg_epoch_time)}/epoch, ETA: {print_time(avg_epoch_time * (args.epochs - epoch - 1))}]"
+                f"[tr.Loss: {train_loss * args.alpha / (batch_idx + 1):.03f}]"
+                f"[tr.Acc: {100.*correct/total:.03f}, {correct} / {total}]",
+                flush=True
+            )
+
+        scheduler.step()
+
+        yield net, epoch + 1, train_loss / (batch_idx + 1), avg_epoch_time
+
+
+def test(args, testloader, net, criterion, print_flag=True):
+
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(args.device), targets.to(args.device)
+            outputs = net(inputs)
+
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+
+            correct, total = measure_accuracy(args, outputs, targets, correct, total)
+
+        if print_flag:
+            print(
+                f"[TEST][te.Loss: {test_loss * args.alpha / (batch_idx + 1):.03f}]"
+                f"[te.Acc: {100. * correct / total:.03f}, {correct} / {total}]",
+                flush=True
+            )
+
+    return 100.0 * correct / total
+
+
+# timing function
+def print_time(elapsed_time):
+
+    # if less than a second, print milliseconds
+    if elapsed_time < 1:
+        return f"{elapsed_time * 1000:.00f}ms"
+
+    elapsed_seconds = round(elapsed_time)
+
+    m, s = divmod(elapsed_seconds, 60)
+    h, m = divmod(m, 60)
+
+    elapsed_time = []
+    if h > 0:
+        elapsed_time.append(f"{h}h")
+    if not (h == 0 and m == 0):
+        elapsed_time.append(f"{m:02}m")
+    elapsed_time.append(f"{s:02}s")
+
+    return "".join(elapsed_time)
+
+
+def weights_evolution(f0, f):
+    s0 = f0.state_dict()
+    s = f.state_dict()
+    nd = 0
+    for k in s:
+        nd += (s0[k] - s[k]).norm() / s0[k].norm()
+    nd /= len(s)
+    return nd
+
+
+
+
+def experiment_initialization(args):
+    """
+        Initialize dataset and architecture.
+    """
+    torch.manual_seed(args.seed_init)
+
+    trainset, testset, input_dim, ch = dataset_initialization(args)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+
+    if testset:
+        testloader = torch.utils.data.DataLoader(
+            testset, batch_size=100, shuffle=False, num_workers=0)
+    else:
+        testloader = None
+
+    net = model_initialization(args, input_dim=input_dim, ch=ch)
+
+    return trainloader, testloader, net
+
+def dataset_initialization(args):
+    """
+    Initialize train and test loaders for chosen dataset and transforms.
+    :param args: parser arguments (see main.py)
+    :return: trainloader, testloader, image size, number of classes.
+    """
+
+    nc = args.num_classes
+
+    transform = None
+    if args.auto_regression:
+        # def transform(x, y):
+        #     return x[:-1], x[1:]
+        print('BERT mode')
+        def transform(x, _):
+            """ BERT-like masking. """
+            idx = torch.randint(2 ** args.num_layers, (1,))[0]
+            p = torch.rand(1)[0]
+            y = torch.tensor([idx, x[idx]])
+            x = x.clone()
+            if p > .2:
+                x[idx] = 0
+            elif .1 < p < .2:
+                x[idx] = torch.randint(args.num_features, (1,))[0]
+            return x.contiguous(), y
+            # mask = torch.rand((len(x),)) > args.pmask
+            # return x * mask, x
+
+    if args.dataset == 'hier1':
+        trainset = DataModel(
+            num_features=args.num_features,
+            m=args.m,  # features multiplicity
+            num_layers=args.num_layers,
+            num_classes=nc,
+            s=args.s, # tuples size
+            input_format=args.input_format,
+            whitening=args.whitening,
+            seed=args.seed_init,
+            train=True,
+            transform=transform,
+            testsize=args.pte,
+            max_dataset_size=args.ptr+args.pte
+        )
+
+        if args.pte:
+            testset = DataModel(
+                num_features=args.num_features,
+                m=args.m,  # features multiplicity
+                num_layers=args.num_layers,
+                num_classes=nc,
+                s=args.s, # tuples size
+                input_format=args.input_format,
+                whitening=args.whitening,
+                seed=args.seed_init,
+                train=False,
+                transform=transform,
+                testsize=args.pte,
+                max_dataset_size=args.ptr+args.pte
+            )
+        else:
+            testset = None
+
+    else:
+        raise ValueError('`dataset` argument is invalid!')
+
+    input_dim = trainset[0][0].shape[-1]
+    ch = trainset[0][0].shape[-2] if args.input_format != 'long' else 0
+
+    if args.loss == 'hinge':
+        # change to binary labels
+        trainset.targets = 2 * (torch.as_tensor(trainset.targets) >= nc // 2) - 1
+        if testset:
+            testset.targets = 2 * (torch.as_tensor(testset.targets) >= nc // 2) - 1
+
+    P = len(trainset)
+
+    # take random subset of training set
+    torch.manual_seed(args.seed_trainset)
+    perm = torch.randperm(P)
+    trainset = torch.utils.data.Subset(trainset, perm[:args.ptr])
+
+    return trainset, testset, input_dim, ch
+
+
+def model_initialization(args, input_dim, ch):
+    """
+    Neural netowrk initialization.
+    :param args: parser arguments
+    :return: neural network as torch.nn.Module
+    """
+
+    num_outputs = 1 if args.loss == "hinge" else args.num_classes
+
+    ### Define network architecture ###
+    torch.manual_seed(args.seed_net)
+
+    net = None
+
+    if args.net == "fcn":
+        net = FCN(
+            num_layers=args.net_layers,
+            input_channels=ch * input_dim,
+            h=args.width,
+            out_dim=num_outputs,
+            bias=args.bias,
+        )
+    elif args.net == "fcn2": # (h * space_dimension) neurons per layer instead of h
+        net = FCN2(
+            num_layers=args.net_layers,
+            input_channels=ch,
+            h=args.width,
+            out_dim=num_outputs,
+            bias=args.bias,
+        )
+    elif args.net == "lcn":
+        net = LCN(
+            num_layers=args.net_layers,
+            input_channels=ch,
+            h=args.width,
+            # filter_size=args.filter_size,
+            out_dim=num_outputs,
+            bias=args.bias,
+        )
+    elif args.net == "cnn":
+        net = CNN(
+            num_layers=args.net_layers,
+            input_channels=ch,
+            h=args.width,
+            patch_size=args.filter_size,
+            out_dim=num_outputs,
+            bias=args.bias,
+        )
+
+    assert net is not None, "Network architecture not in the list!"
+
+    if args.random_features:
+        for param in [p for p in net.parameters()][:-2]:
+            param.requires_grad = False
+
+    net = net.to(args.device)
+
+    # if args.device == "cuda":
+    #     net = torch.nn.DataParallel(net)
+    #     cudnn.benchmark = True
+
+    return net
---
+++ CetinSak/src/experiments/exp_1.py
@@ -0,0 +1,69 @@
+import tomllib as toml
+import copy
+from pprint import pprint
+
+from __init__ import experiment_run
+
+class Config:
+    def __init__(self, input_dict) -> None:
+        for top_key, sub_dict in input_dict.items():
+            if isinstance(sub_dict, dict):
+                for sub_key, value in sub_dict.items():
+                    setattr(self, sub_key, value)
+
+
+def load_config(config_path):
+    config_toml = toml.load(config_path)
+
+    assert config_toml["dataset"]["dataset"], "dataset.dataset cannot be None."
+    assert config_toml["model"]["net"], "model.net cannot be None."
+
+    config_enumerated = enumerate_config(config_toml)
+
+    config_args = [Config(config) for config in config_enumerated]
+
+    return config_enumerated, config_args
+
+def enumerate_config(config):
+    
+    dict_stack = [config]
+    dict_results = []
+
+    # Generate different configs from lists
+    while dict_stack:
+        config_dict = dict_stack.pop()
+        has_no_lists = True
+        for top_key, sub_dict in config_dict.items():
+            for sub_key, value in sub_dict.items():
+                if isinstance(value, (list, tuple)) and len(value) > 1:
+                    has_no_lists = False 
+                    print(f"{sub_key} has more than one elements")
+
+                    for elem in value:
+                        config_copy = copy.deepcopy(config_dict)
+                        config_copy[top_key][sub_key] = elem
+                        dict_stack.append(config_copy)
+                if not has_no_lists:
+                    break
+            if not has_no_lists:
+                break
+        
+        if has_no_lists:
+            dict_results.append(config_dict)
+
+    # Fix one element list and tuples
+    for sub_config in dict_results:
+        for sub_dict in sub_config.values():
+            for key, value in sub_dict.items():
+                if isinstance(value, (list, tuple)):
+                    sub_dict[key] = value[0]    
+    
+    return dict_results
+
+if __name__ == "__main__":
+    config_dict_list, config_args_list = load_config("exp1_config.toml")
+
+    for config_dict, args in zip(config_dict_list, config_args_list):
+        print("Running experiment with following config:")
+        pprint(config_dict)
+        experiment_run(args)
