b01423504facd28770e9cfc5d8a6ff44ed324ebd
erinc <burakerinc@gmail.com>
2025-01-09 22:54:43 +0300

    fix diffeo/synonym test set bug

---
+++ CetinSak/src/plotter.py
@@ -0,0 +1,13 @@
+# take 1 exp output -> plot graph
+
+# graph types:
+# 1 - normal train graph
+# 2 - sensitivity vs to %90 accuracy
+# 3 - sensitivity vs current accuracy 
+# 4 - complexity vs %90 accuracy
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
